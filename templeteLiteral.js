b92ee06a88ce8daf26a3c2449865958631761fcd
Jahirul Islam <jahirulislamjantu002@gmail.com>
2023-11-30 21:41:14 +0600

    first commit

--- templeteLiteral.js
+++ templeteLiteral.js
@@ -1,5 +1,9 @@
-const productName = 'Mobile Phone';
-const productPrice = 1000;
-const date = '8/27/2023';
+// Template Literal sample JS
 
-console.log(`You have buy a ${productName}, your ${productName} price is ${productPrice} BDT. Your purchase date is ${date}. Have a good day sir.`)
+const productName = "Mobile Phone";
+const productPrice = 1000;
+const date = "8/27/2023";
+
+console.log(
+  `You have buy a ${productName}, your ${productName} price is ${productPrice} BDT. Your purchase date is ${date}. Have a good day sir.`
+);
