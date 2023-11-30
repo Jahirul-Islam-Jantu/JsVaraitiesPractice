266492453e2c60d0ab673f4c3ae31383f99b1523
Jahirul Islam <jahirulislamjantu002@gmail.com>
2023-11-30 21:32:57 +0600

    first commit

--- forloop.js
+++ forloop.js
@@ -1,25 +1,26 @@
-for (let oddNumber = 1; oddNumber <=5; oddNumber +=1){
-    if (oddNumber % 2){
-        console.log(`${oddNumber} is a Odd Number.`)
-    }else {
-        `${oddNumber}is a Even Number.`
-    }
+// For loops concepts
+
+for (let oddNumber = 1; oddNumber <= 5; oddNumber += 1) {
+  if (oddNumber % 2) {
+    console.log(`${oddNumber} is a Odd Number.`);
+  } else {
+    `${oddNumber}is a Even Number.`;
+  }
 }
 
-for (let evenNumber = 0; evenNumber <=6; evenNumber +=1){
-    if (evenNumber % 2 === 1){
-        console.log(`${evenNumber} is a Odd Number.`)
-    }
-    // else {
-    //    console.log (`${evenNumber} is a Even Number.`)
-    // }
+for (let evenNumber = 0; evenNumber <= 6; evenNumber += 1) {
+  if (evenNumber % 2 === 1) {
+    console.log(`${evenNumber} is a Odd Number.`);
+  }
+  // else {
+  //    console.log (`${evenNumber} is a Even Number.`)
+  // }
 }
 
-for (let evenNumber = 0; evenNumber <=100; evenNumber +=1){
-    if (evenNumber % 2 === 1){
-        console.log(`${evenNumber} is a Odd Number.`)
-    }
-    else {
-       console.log (`${evenNumber} is a Even Number.`)
-    }
+for (let evenNumber = 0; evenNumber <= 100; evenNumber += 1) {
+  if (evenNumber % 2 === 1) {
+    console.log(`${evenNumber} is a Odd Number.`);
+  } else {
+    console.log(`${evenNumber} is a Even Number.`);
+  }
 }
